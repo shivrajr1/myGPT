4a804839cfb1e8d294d7321dcb9371c8ec34a30c
shivraj <shivrajrajhar9319@gmail.com>
2026-01-08 20:11:17 +0530

    ading toggle on user icon

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useContext } from 'react'
+import { useEffect, useContext, useState } from 'react'
 import './Sidebar.css'
 import context from './context'
 import { func, getThread, dlt } from './utils/sidbar'
@@ -14,6 +14,7 @@ export default function Sidebar() {
     setTotalThread,
     loading,
     setLoading } = useContext(context)
+  const [activeThread, setActiveThread]=useState(null)
 
 
   useEffect(() => {
@@ -25,21 +26,30 @@ export default function Sidebar() {
     <section className="sidebar">
       <div className="sidebar-icon">
         <span className='icon'>
-          <i className="fa-brands fa-openai" onClick={() => setThread({})}></i>
+          <i className="fa-brands fa-openai" 
+          onClick={() => {setThread({});setActiveThread(null)}}></i>
         </span>
         <span className='icon'>
-          <i className="fa-solid fa-pen-to-square" onClick={() => setThread({})}></i>
+          <i className="fa-solid fa-pen-to-square" 
+          onClick={() => {setThread({});setActiveThread(null)}}></i>
         </span>
       </div>
       <ul className="thread">
         {
           totalThread?.map((obj, idx) => {
-            return <li key={idx} onClick={() => { getThread({ id: obj._id, setThread, setTotalThread }) }}>
+            return <li 
+            key={idx} 
+            onClick={() => { 
+              getThread({ id: obj._id, setThread, setTotalThread }) 
+              setActiveThread(idx)
+            }} 
+            className={activeThread === idx ? 'activeThread' : ''}>
               {obj.title}
               <i className="fa-solid fa-trash"
                 onClick={(e) => {
                   e.stopPropagation();
                   dlt({ id: obj._id, setTotalThread, setThread })
+                  setActiveThread(null)
                 }}></i></li>
           })
         }
